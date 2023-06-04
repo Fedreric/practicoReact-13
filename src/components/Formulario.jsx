f891a5cf71c991fcb8effc26123c0aaed38fecaa
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-04 13:35:58 -0300

    Se hace una peticion a la API al precionar el boton

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 import { Container, Form, Button, FloatingLabel } from "react-bootstrap";
 
 const Formulario = () => {
-    const [localidad, setLocalidad] = useState('');
-
+    const [pais, setPais] = useState('');
+    const [datos, setDatos] = useState({});
+    const consultarApi = async (pais) => {
+        try {
+          const respuesta = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${pais}&appid=436b5789015431fd04ad33ddb1024d71`
+          );
+          const dato = await respuesta.json();
+          setDatos(dato);
+        } catch (e) {
+          console.log(e);
+        }
+      };
   return (
     <Container className="my-5">
-      <Form className="row align-items-center">
+      <Form className="row align-items-center" onSubmit={(e)=>{
+        e.preventDefault();
+        consultarApi(pais);
+        console.log(datos)
+      }}>
         <Form.Group className="col-md-10" controlId="pais">
-          <FloatingLabel controlId="pais" label="Ingrese una localidad">
-            <Form.Control type="text" placeholder="Ej: Argentina" onChange={(e)=>setLocalidad(e.target.value)} value={localidad}/>
+          <FloatingLabel controlId="pais" label="Ingrese un pais">
+            <Form.Control type="text" placeholder="Ej: Argentina" onChange={(e)=>setPais(e.target.value)} value={pais}/>
           </FloatingLabel>
         </Form.Group>
         <div className="col-md-2">
