e92c2208b3b3a4cb5664202e52761daeabe0282a
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-04 16:26:04 -0300

    Se agrega el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import Formulario from './components/Formulario';
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import Formulario from "./components/Formulario";
+import FooterFedeLedesma from "./components/FooterFedeLedesma"
+import "./App.css";
 
 function App() {
   return (
-    <Container>
-      <h1 className='display-4 text-center mt-5'> Clima en tiempo real</h1>
-      <hr />
-    <Formulario></Formulario>
-    </Container>
-  )
+    <>
+      <Container className="mainPage">
+        <h1 className="display-4 text-center mt-5"> Clima en tiempo real</h1>
+        <hr />
+        <Formulario></Formulario>
+      </Container>
+      <FooterFedeLedesma></FooterFedeLedesma>
+    </>
+  );
 }
 
-export default App
+export default App;
