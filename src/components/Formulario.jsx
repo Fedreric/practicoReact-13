57838c9932a4a7237cba3a3affcd8d329ee51390
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-04 12:15:50 -0300

    Se corrigen detalles de maquetado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import { Container, Form, Button, FloatingLabel } from "react-bootstrap";
 
 const Formulario = () => {
+    const [localidad, setLocalidad] = useState('');
+
   return (
     <Container className="my-5">
       <Form className="row align-items-center">
         <Form.Group className="col-md-10" controlId="pais">
           <FloatingLabel controlId="pais" label="Ingrese una localidad">
-            <Form.Control type="text" placeholder="Ej: Argentina" />
+            <Form.Control type="text" placeholder="Ej: Argentina" onChange={(e)=>setLocalidad(e.target.value)} value={localidad}/>
           </FloatingLabel>
         </Form.Group>
         <div className="col-md-2">
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" className="w-100 mt-4 mt-md-0">
             Ver clima
           </Button>
         </div>
